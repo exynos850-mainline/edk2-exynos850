8ffb8b6513181412c9e9045fb5f7208884b4c204
SuperAviation001 <superaviation001@gmail.com>
2026-09-05 16:47:35 +1000

    firstrun: revert edk2 to 202508

--- firstrun.sh
+++ firstrun.sh
@@ -3,6 +3,20 @@
 # do this first:
 # https://github.com/tianocore/tianocore.github.io/wiki/Using-EDK-II-with-Native-GCC#Install_required_software_from_apt
 set -e
+
+cd ../edk2
+git submodule deinit -f --all
+git clean -ffxd
+git checkout tags/edk2-stable202508 -b build-compat-2025
+git submodule update --init --recursive
+
+cd ../edk2-platforms
+git fetch origin
+git checkout $(git rev-list -n 1 --before="2025-08-22" origin/master)
+git submodule update --init --recursive
+
+cd ../edk2-exynos850
+
 . build_common.sh
 make -C ../edk2/BaseTools
 
